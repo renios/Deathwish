80a65ac172390162a08059c05e62269dc10fbf63
Minjung Kim <Reny.atkins@gmail.com>
2015-06-21 21:03:26 +0900

    Implement ESC popup menu

---
+++ Assets/ESCMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ESCMenu : MonoBehaviour {
+
+	public GameObject PopupMenu;
+
+	void Awake () {
+		PopupMenu.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Input.GetKeyDown("escape")) && (PopupMenu.activeInHierarchy == false))
+		{
+			PopupMenu.SetActive(true);
+		}
+		// Using test.
+		else if ((Input.GetKeyDown("escape")) && (PopupMenu.activeInHierarchy == true))
+		{
+			PopupMenu.SetActive(false);
+		}
+	}
+}
