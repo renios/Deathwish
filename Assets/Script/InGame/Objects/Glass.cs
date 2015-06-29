25227b54cbb5af0b1fadd29d23219f91267b59b2
noisum <seomated@naver.com>
2015-06-29 22:18:29 +0900

    implemented glass

---
+++ Assets/Script/InGame/Objects/Glass.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glass : MonoBehaviour
+{
+	private BoxCollider2D coll;
+
+	void Start()
+	{
+		coll = GetComponent<BoxCollider2D> ();
+	}
+
+	void Update()
+	{
+		if(Global.ingame.isDark == Enums.IsDark.Light)
+		{
+			coll.enabled = true;
+		}
+		else if(Global.ingame.isDark == Enums.IsDark.Dark)
+		{
+			coll.enabled = false;
+		}
+	}
+}
