a90b36485484cf6f9e7b0370da16cf3fed3992b9
Minjung Kim <Reny.atkins@gmail.com>
2015-07-04 17:30:56 +0900

    temp commit

--- Assets/Script/ESCMenu.cs
+++ Assets/Script/ESCMenu.cs
@@ -14,6 +14,7 @@ public class ESCMenu : MonoBehaviour {
 		if ((Input.GetKeyDown("escape")) && (PopupMenu.activeInHierarchy == false))
 		{
 			PopupMenu.SetActive(true);
+			//GameObject.Find("MainMenu").GetComponent<Button>().OnPointerEnter();
 		}
 		// Using test.
 		else if ((Input.GetKeyDown("escape")) && (PopupMenu.activeInHierarchy == true))
