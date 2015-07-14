1280987a22f4d7e5aff1308f0ae9bfa48704e2a7
EQwon <bluetwintail0906@gmail.com>
2015-07-15 01:56:04 +0900

    Smoke 제작 + Mirror 수정
    연기 오브젝트 제작했습니다.
    거울 배경 제거했습니다.

---
+++ Assets/Script/InGame/Objects/Unpenetrate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Enums;
+
+public class Unpenetrate : MonoBehaviour
+{
+	public Sprite inlight;
+	public Sprite indark;
+	private BoxCollider2D coll;
+	
+	void Start()
+	{
+		coll = GetComponent<BoxCollider2D> ();
+	}
+	
+	void Update()
+	{
+		if(Global.ingame.isDark == IsDark.Light)
+		{
+			coll.enabled = false;
+			gameObject.GetComponent<SpriteRenderer>().sprite = inlight;
+		}
+		else if(Global.ingame.isDark == IsDark.Dark)
+		{
+			coll.enabled = true;
+			gameObject.GetComponent<SpriteRenderer>().sprite = indark;
+		}
+	}
+}
