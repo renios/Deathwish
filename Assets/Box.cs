b810f229b261243613a969945a470ced51120a71
Minjung Kim <Reny.atkins@gmail.com>
2015-07-21 22:27:55 +0900

    Implement Box object.

---
+++ Assets/Box.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Enums;
+
+public class Box : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Global.ingame.isDark == IsDark.Light)
+		{
+			gameObject.GetComponent<Rigidbody2D>().isKinematic = false;	
+		}
+		else if (Global.ingame.isDark == IsDark.Dark)
+		{
+			gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+		}
+	}
+}
