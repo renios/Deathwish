cb17d83cb7f89d65cee8c8169a612f5e7ead543f
Minjung Kim <Reny.atkins@gmail.com>
2015-07-23 01:10:15 +0900

    Player CAN pass doors. Now 'isTrigger' of doors is TRUE value

--- Assets/Script/InGame/Objects/Goal.cs
+++ Assets/Script/InGame/Objects/Goal.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Goal : MonoBehaviour
 {
-	void OnCollisionEnter2D(Collision2D collision)
+	void OnTriggerEnter2D(Collider2D collision)
 	{
 		if(collision.gameObject.tag == "Player")
 		{
