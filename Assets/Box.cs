9192d932c8fcce2d664a4f34541309db5954196c
Minjung Kim <Reny.atkins@gmail.com>
2015-07-22 22:16:26 +0900

    Fix features of box

--- Assets/Box.cs
+++ Assets/Box.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 using Enums;
 
-public class Box : MonoBehaviour {
+public class Box : MonoBehaviour, IRestartable {
 
+	private Vector3 originalPosition;
+	
 	// Use this for initialization
 	void Start () {
-	
+		originalPosition = gameObject.transform.position;
 	}
 	
 	// Update is called once per frame
@@ -19,5 +21,12 @@ public class Box : MonoBehaviour {
 		{
 			gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
 		}
+	}
+	
+	void IRestartable.Restart()
+	{
+		gameObject.transform.position = originalPosition;
+		gameObject.GetComponent<Rigidbody2D>().isKinematic = false;		
+		gameObject.GetComponent<SpriteRenderer>().sprite = gameObject.GetComponent<SpriteSwitch>().light;
 	}
 }
