8e2773089ce5bdff58aa4b524195db45ff35cb97
EQwon <bluetwintail0906@gmail.com>
2015-07-28 01:11:02 +0900

    Make CameraController
    Make CameraController.cs

--- Assets/Script/InGame/CameraController.cs
+++ Assets/Script/InGame/CameraController.cs
@@ -3,16 +3,45 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-	public Player player;
-	private Vector3 fromPlayerToCamera;
+	public GameObject player;
+	public float xLimit;
+	public float yLimit;
+
+	private Vector3 startPoint;
 
 	void Start ()
 	{
-		fromPlayerToCamera = player.gameObject.transform.position - gameObject.transform.position;
+		startPoint = player.transform.position - new Vector3 (0,0,10);
+
+		gameObject.transform.position = startPoint;
 	}
-	
-	void LateUpdate ()
+
+	void Update ()
 	{
-		gameObject.transform.position = player.transform.position - fromPlayerToCamera;
+		Vector3 cameraPosition = gameObject.transform.position;
+		float playerX = player.transform.position.x;
+		float playerY = player.transform.position.y;
+
+		if (cameraPosition.x - playerX > xLimit)
+		{
+			cameraPosition.x = playerX + xLimit;
+		}
+
+		if (cameraPosition.x + xLimit < playerX)
+		{
+			cameraPosition.x = playerX - xLimit;
+		}
+
+		if (cameraPosition.y - playerY > yLimit)
+		{
+			cameraPosition.y = playerY + yLimit;
+		}
+
+		if (cameraPosition.y + yLimit < playerY)
+		{
+			cameraPosition.y = playerY - yLimit;
+		}
+
+		gameObject.transform.position = cameraPosition;
 	}
 }
