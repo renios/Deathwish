fe0c238be7603993054343f76eae4910f39bf5f3
Minjung Kim <Reny.atkins@gmail.com>
2015-07-29 21:08:28 +0900

    Add script showing 'LightState'.

---
+++ Assets/Script/InGame/Objects/LightState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightState : MonoBehaviour {
+	
+	public enum IsLight
+	{
+		True,
+		False
+	}
+
+	private IsLight lightState;
+	
+	public IsLight GetLightState()
+	{
+		return lightState; 
+	}
+
+	public void ChangeStateToLight()
+	{
+		lightState = IsLight.True;
+	}
+	
+	public void ChangeStateToDark()
+	{
+		lightState = IsLight.False;
+	}
+
+	void InitiateToLight()
+	{
+		lightState = IsLight.True;
+	}
+
+	// Use this for initialization
+	void Start () {
+		InitiateToLight();
+	}
+}
