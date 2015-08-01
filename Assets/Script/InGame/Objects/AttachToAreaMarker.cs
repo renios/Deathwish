c9ae4ffb323098169754ccd8719d52a60c7c25ed
Minjung Kim <Reny.atkins@gmail.com>
2015-08-02 02:05:16 +0900

    Add script to AreaMarker.

---
+++ Assets/Script/InGame/Objects/AttachToAreaMarker.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Enums;
+
+public class AttachToAreaMarker : MonoBehaviour {
+
+	private ObjectType parentObjectType;
+
+	public void SetParentObjectType(ObjectType parentObjectType)
+	{
+		this.parentObjectType = parentObjectType;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "AreaMarker")
+		{
+			GameObject areaMarker = other.gameObject;
+			if (parentObjectType == ObjectType.LightBug)
+			{
+				foreach(Collider2D colliderOfOther in Physics2D.OverlapAreaAll(areaMarker.GetComponent<Collider2D>().bounds.max, areaMarker.GetComponent<Collider2D>().bounds.min))
+				{
+					if ((colliderOfOther.gameObject.GetComponent<LightState>() != null) && (colliderOfOther.gameObject.tag != "LightBug") && (colliderOfOther.gameObject.tag != "Player"))
+						colliderOfOther.gameObject.GetComponent<LightState>().AttachLightBug();
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "AreaMarker")
+		{
+			GameObject areaMarker = other.gameObject;
+			if (parentObjectType == ObjectType.LightBug)
+			{
+				foreach(Collider2D colliderOfOther in Physics2D.OverlapAreaAll(areaMarker.GetComponent<Collider2D>().bounds.max, areaMarker.GetComponent<Collider2D>().bounds.min))
+				{
+					if ((colliderOfOther.gameObject.GetComponent<LightState>() != null) && (colliderOfOther.gameObject.tag != "LightBug") && (colliderOfOther.gameObject.tag != "Player"))
+						colliderOfOther.gameObject.GetComponent<LightState>().DetachLightBug();
+				}
+			}
+		}
+	}
+
+}
