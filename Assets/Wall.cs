0c05a9b912841a96f192a1f4cee61ed6cac34b10
Minjung Kim <Reny.atkins@gmail.com>
2015-08-02 02:09:43 +0900

    Add script to Wall object. It is relate to lightbug

---
+++ Assets/Wall.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if ((GetComponent<LightState>().GetLightState() == LightState.IsLight.False) &&
+			(GetComponent<LightState>().IsAttachedLightBug() == LightState.AttachedLightBug.True))
+			GetComponent<Collider2D>().enabled = false;
+		else
+			GetComponent<Collider2D>().enabled = true;
+	}
+}
