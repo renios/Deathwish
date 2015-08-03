0e70ec1f003aafb5841dc630181cb5ef81805b6b
Minjung Kim <Reny.atkins@gmail.com>
2015-08-03 23:28:01 +0900

    Devide changing sprite methods to Penetrate class.

--- Assets/Script/InGame/Objects/Penetrate.cs
+++ Assets/Script/InGame/Objects/Penetrate.cs
@@ -4,9 +4,6 @@ using Enums;
 
 public class Penetrate : ObjectMonoBehaviour
 {
-	public Sprite inLight;
-	public Sprite inDark;
-
 	private BoxCollider2D coll;
 	// Read at Editor (Inspector). (ex : is Transparent In Light / Dark)
 	public IsDark isTransparentIn;
@@ -19,15 +16,6 @@ public class Penetrate : ObjectMonoBehaviour
 	public override void UpdateByParent()
 	{
 		GetComponent<SpriteRenderer>().enabled = true;
-
-		if(Global.ingame.isDark == IsDark.Light)
-		{
-			gameObject.GetComponent<SpriteRenderer>().sprite = inLight;
-		}
-		else if(Global.ingame.isDark == IsDark.Dark)
-		{
-			gameObject.GetComponent<SpriteRenderer>().sprite = inDark;
-		}
 
 		if (Global.ingame.isDark == isTransparentIn)
 		{
