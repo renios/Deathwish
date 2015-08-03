d3df5c79fa5fc84289d3e0e7d608266e691fae46
Minjung Kim <Reny.atkins@gmail.com>
2015-08-03 23:15:22 +0900

    Remove variable and methods are related with LightState.

--- Assets/Script/InGame/Objects/SpriteSwitch.cs
+++ Assets/Script/InGame/Objects/SpriteSwitch.cs
@@ -17,21 +17,13 @@ public class SpriteSwitch : MonoBehaviour
 
 	void Update ()
 	{
-		if ((GetComponent<LightState>() != null) && 
-			(GetComponent<LightState>().GetLightState() == LightState.IsLight.False) &&
-			(GetComponent<LightState>().IsAttachedLightBug() == LightState.AttachedLightBug.True))
-			sr.enabled = false;
-		else
+		if(Global.ingame.isDark == IsDark.Light)
 		{
-			sr.enabled = true;
-			if(Global.ingame.isDark == IsDark.Light)
-			{
-				sr.sprite = light;
-			}
-			else if(Global.ingame.isDark == IsDark.Dark)
-			{
-				sr.sprite = dark;
-			}
+			sr.sprite = light;
+		}
+		else if(Global.ingame.isDark == IsDark.Dark)
+		{
+			sr.sprite = dark;
 		}
 	}
 }
