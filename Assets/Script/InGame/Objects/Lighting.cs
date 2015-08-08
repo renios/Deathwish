f69ec6250dab609007bcb520fad26d15f910d304
EQwon <bluetwintail0906@gmail.com>
2015-08-09 02:13:13 +0900

    Make Lighting Script - Temporarily

---
+++ Assets/Script/InGame/Objects/Lighting.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using System.Collections;
+using System;
+using Enums;
+
+public class Lighting: MonoBehaviour, IRestartable
+{
+	public new Sprite light;
+	public Sprite dark;
+
+	bool isLighting;
+
+	void Start()
+	{
+		isLighting = false;
+		repeat ();
+	}
+
+	void changeLight()
+	{
+		isLighting = true;
+		GetComponent<SpriteRenderer> ().sprite = light;
+	}
+	void changeDark()
+	{
+		isLighting = false;
+		GetComponent<SpriteRenderer> ().sprite = dark;
+	}
+
+	void repeat()
+	{
+		InvokeRepeating ("changeDark", 0, 10);
+		InvokeRepeating ("changeLight", 5, 10);
+	}
+
+	void IRestartable.Restart()
+	{
+		Global.ingame.isDark = IsDark.Light;
+	}
+
+}
