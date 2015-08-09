a47c6dfbae3b1e8dc3690f55c17a9de63201733e
EQwon <bluetwintail0906@gmail.com>
2015-08-09 09:11:51 +0900

    Modify Lighting Script - Still Modifing

--- Assets/Script/InGame/Objects/Lighting.cs
+++ Assets/Script/InGame/Objects/Lighting.cs
@@ -8,13 +8,33 @@ public class Lighting: MonoBehaviour, IRestartable
 {
 	public new Sprite light;
 	public Sprite dark;
+	public LightingChecker lightingChecker;
+
+	private int length;
+	
+	GameObject[] effectGameObjects;
 
 	bool isLighting;
+	bool isEffectLighting;
 
 	void Start()
 	{
+		effectGameObjects = GetComponentInChildren<LightingChecker> ().lightingEffectObjects;
 		isLighting = false;
+		length = effectGameObjects.Length;
 		repeat ();
+	}
+
+	void Update()
+	{
+		if (lightingChecker.IsEffectLighting () && Global.ingame.isDark == IsDark.Dark && isLighting == true)
+		{
+			for(int i = 0; i < length; i++)
+			{
+				if (effectGameObjects[i].GetComponent<SpriteSwitch>() != null)
+					effectGameObjects[i].GetComponent<SpriteSwitch>().isDark = IsDark.Light;
+			}
+		}
 	}
 
 	void changeLight()
