716b3215dabc7ad7614724a760d7434a4cd4b2bf
Minjung Kim <Reny.atkins@gmail.com>
2015-08-09 21:54:08 +0900

    Apply light particles

--- Assets/Script/InGame/Objects/Lamp.cs
+++ Assets/Script/InGame/Objects/Lamp.cs
@@ -7,16 +7,28 @@ public class Lamp : ObjectMonoBehaviour
 {
 	public LampProperty lampProperty;
 	public float detectingRadius;
+	public GameObject LightParticleObject;
+	public GameObject DarkParticleObject;
+	private ParticleSystem particle;
 	//forDebugging
 	List<Lamp> lamps;
-	Scaler scaler;
 
 	void Start()
 	{
 		Global.ingame.LampsInMap.Add (this);
 		lamps = new List<Lamp>(Global.ingame.LampsInMap);
-		scaler = GetComponentInChildren<Scaler> ();
-		scaler.detectingRadius = detectingRadius;
+		
+		if (lampProperty == LampProperty.LightLamp)
+		{
+			particle = LightParticleObject.GetComponent<ParticleSystem>();
+			DarkParticleObject.SetActive(false);
+		}
+		else if (lampProperty == LampProperty.DarkLamp)
+		{
+			particle = DarkParticleObject.GetComponent<ParticleSystem>();
+			LightParticleObject.SetActive(false);
+		}
+		particle.startSize *= detectingRadius;
 	}
 
 	public override void UpdateByParent ()
