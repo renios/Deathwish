9e3d9321ddbe908d4e4fdedf86a7dc152a525166
Seung Woo Kim <koreanboy1120@gmail.com>
2015-08-10 16:32:44 +0900

    Script to allow saving and get rid of locks for individual stages.

---
+++ Assets/Script/InGame/LevelSelectManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelectManager : MonoBehaviour
+{
+
+	public string[] LevelTags;
+
+	public GameObject[] Locks;
+	public bool[] LevelUnlocked;
+
+	void Start()
+	{
+		for (int i = 0; i < LevelTags.Length; i++)
+		{
+			if (PlayerPrefs.GetInt(LevelTags[i]) == null)
+			{
+				LevelUnlocked[i] = false;
+			}
+			else if (PlayerPrefs.GetInt(LevelTags[i]) == 0)
+			{
+				LevelUnlocked[i] = false;
+			}
+			else
+			{
+				LevelUnlocked[i] = true;
+			}
+
+			if (LevelUnlocked[i])
+			{
+				Locks[i].SetActive(false);
+			}
+		}
+	}
+
+	void Update()
+	{
+		
+	}
+
+}
