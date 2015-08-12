11029b1590f64d78e4485209e0b12c786e450a3c
EQwon <bluetwintail0906@gmail.com>
2015-08-12 22:12:18 +0900

    Modify Bug_29
    Add GroundChecker to Mirror

--- Assets/Script/InGame/Objects/SwitchDarkLight.cs
+++ Assets/Script/InGame/Objects/SwitchDarkLight.cs
@@ -4,9 +4,11 @@ using Enums;
 
 public class SwitchDarkLight : MonoBehaviour
 {
+	public GroundChecker groundChecker;
+
 	void OnTriggerStay2D(Collider2D collision)
 	{
-		if ((collision.gameObject.tag == "Player") && (Input.GetKeyDown(KeyCode.UpArrow)))
+		if (groundChecker.IsGrounded() && (collision.gameObject.tag == "Player") && (Input.GetKeyDown(KeyCode.UpArrow)))
 		{
 			Global.ingame.ChangeDarkLight();
 		}
