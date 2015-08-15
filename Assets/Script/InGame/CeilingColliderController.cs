2c8762df50d4ce30dc2a96dc28f90fb7a325838e
EQwon <bluetwintail0906@gmail.com>
2015-08-16 00:36:56 +0900

    Chagne CCC.cs for GroundwithLadder's correction
    사다리 땅의 콜라이더를 edge 콜라이더로 바꾸면서 콜라이더 2개를 모두 off 해야 해서 array로 바꾸었습니다.

--- Assets/Script/InGame/CeilingColliderController.cs
+++ Assets/Script/InGame/CeilingColliderController.cs
@@ -4,20 +4,22 @@ using System.Collections;
 public class CeilingColliderController : MonoBehaviour
 {
 	public GameObject Ground;
-	new Collider2D collider2D;
+	new Collider2D[] collider2D;
 
 	void Start()
 	{
-		collider2D = Ground.GetComponent<Collider2D> ();
+		collider2D = Ground.GetComponents<Collider2D> ();
 	}
 
 	public void DisableCeiling()
 	{
-		collider2D.enabled = false;
+		collider2D [0].enabled = false;
+		collider2D [1].enabled = false;
 	}
 
 	public void EnableCeiling()
 	{
-		collider2D.enabled = true;
+		collider2D [0].enabled = true;
+		collider2D [1].enabled = true;
 	}
 }
