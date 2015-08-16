08ddc65d950c5f7b5066f20210c2fe0bf0c5730c
ParkJuhyeong <majecty@gmail.com>
2015-08-16 09:22:21 +0900

    Implement scolling background.

---
+++ Assets/Script/Background.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour
+{
+    float leftX;
+    float rightX;
+    float downY;
+    float upY;
+    Player player;
+
+    Vector3 cameraInitialPos = Vector3.zero;
+    public Transform cameraTransform;
+
+    void Start()
+    {
+        leftX = ObjectFinder.FindLeftmost().transform.position.x;
+        rightX = ObjectFinder.FindRightmost().transform.position.x;
+        downY = ObjectFinder.FindLowest().transform.position.y;
+        upY = ObjectFinder.FindUpmost().transform.position.y;
+        player = Global.ingame.GetPlayer();
+        cameraInitialPos = cameraTransform.transform.position;
+    }
+
+    void Update()
+    {
+        var middleX = (rightX + leftX) / 2;
+        var ratioX = (player.transform.position.x - middleX) / (rightX - leftX);
+        var diffPosX = ratioX * 8; // (400 pixel() / (50 pixel per unit)) = 8 
+
+        var middleY = (upY + downY) / 2;
+        var ratioY = (player.transform.position.y - middleY) / (upY - downY);
+        var diffPosY = ratioY * 5; // (250 pixel() / (50 pixel per unit)) = 8 
+        cameraTransform.transform.position = new Vector3(cameraInitialPos.x + diffPosX,
+        cameraInitialPos.y + diffPosY, cameraInitialPos.z);
+    }
+}
---
+++ Assets/Script/ObjectFinder.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class ObjectFinder
+{
+    public static Transform FindLowest()
+    {
+        Transform transformOfLowestObject;
+
+        Transform[] allTransforms = GameObject.FindObjectsOfType<ObjectMonoBehaviour>()
+            .Select(objectMonoBehaviour => objectMonoBehaviour.GetComponent<Transform>()).ToArray();
+        transformOfLowestObject = allTransforms[0];
+
+        foreach (Transform transform in allTransforms)
+        {
+            if (transformOfLowestObject.position.y >= transform.position.y)
+            {
+                transformOfLowestObject = transform;
+            }
+        }
+        return transformOfLowestObject;
+    }
+
+    public static Transform FindRightmost()
+    {
+        Transform transformOfLowestObject;
+
+        Transform[] allTransforms = GameObject.FindObjectsOfType<ObjectMonoBehaviour>()
+            .Select(objectMonoBehaviour => objectMonoBehaviour.GetComponent<Transform>()).ToArray();
+        transformOfLowestObject = allTransforms[0];
+
+        foreach (Transform transform in allTransforms)
+        {
+            if (transformOfLowestObject.position.x <= transform.position.x)
+            {
+                transformOfLowestObject = transform;
+            }
+        }
+        return transformOfLowestObject;
+    }
+
+    public static Transform FindLeftmost()
+    {
+        Transform transformOfLowestObject;
+
+        Transform[] allTransforms = GameObject.FindObjectsOfType<ObjectMonoBehaviour>()
+            .Select(objectMonoBehaviour => objectMonoBehaviour.GetComponent<Transform>()).ToArray();
+        transformOfLowestObject = allTransforms[0];
+
+        foreach (Transform transform in allTransforms)
+        {
+            if (transformOfLowestObject.position.x >= transform.position.x)
+            {
+                transformOfLowestObject = transform;
+            }
+        }
+        return transformOfLowestObject;
+    }
+
+    public static Transform FindUpmost()
+    {
+        Transform transformOfLowestObject;
+
+        Transform[] allTransforms = GameObject.FindObjectsOfType<ObjectMonoBehaviour>()
+            .Select(objectMonoBehaviour => objectMonoBehaviour.GetComponent<Transform>()).ToArray();
+        transformOfLowestObject = allTransforms[0];
+
+        foreach (Transform transform in allTransforms)
+        {
+            if (transformOfLowestObject.position.y <= transform.position.y)
+            {
+                transformOfLowestObject = transform;
+            }
+        }
+        return transformOfLowestObject;
+    }
+}
