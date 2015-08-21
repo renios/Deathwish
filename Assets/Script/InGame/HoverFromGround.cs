108821e17d852c0115e4938505d02cac37bc8f82
ParkJuhyeong <majecty@gmail.com>
2015-08-22 00:23:20 +0900

    Remove blank line in codes.

--- Assets/Script/InGame/HoverFromGround.cs
+++ Assets/Script/InGame/HoverFromGround.cs
@@ -2,18 +2,18 @@
 using System.Collections;
 
 public class HoverFromGround : MonoBehaviour {
-	
+
 	public LayerMask groundMask;
 	private float depth = 0.02f;
 
-	void FixedUpdate() {		
+	void FixedUpdate() {
 		var minY = GetComponent<BoxCollider2D>().bounds.min.y;
 		var castResult = Physics2D.Raycast(new Vector2(transform.position.x, minY), Vector2.down, 0.5f, groundMask);
 		if (castResult.collider == null)
 		{
 			return;
 		}
-		
+
 		var diff = minY - castResult.point.y;
 		if (0 < diff && diff < depth)
 		{
