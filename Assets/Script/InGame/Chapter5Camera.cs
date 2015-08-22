782fbe794aa222406e0c1dc1b73d966876d55727
ParkJuhyeong <majecty@gmail.com>
2015-08-23 00:22:35 +0900

    Implement middle camera.

---
+++ Assets/Script/InGame/Chapter5Camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chapter5Camera : MonoBehaviour {
+	void LateUpdate()
+	{
+		Player[] players = GameObject.FindObjectsOfType<Player>();
+		float totalX = 0;
+		float totalY = 0;
+		
+		foreach (var player in players)
+		{
+			totalX += player.transform.position.x;
+			totalY += player.transform.position.y;
+		}
+		
+		float averageX = totalX / players.Length;
+		float averageY = totalY / players.Length;
+		
+		transform.position = new Vector3(averageX, averageY, transform.position.z);
+	}
+}
