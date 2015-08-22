644b2b19239236f3c00192ba09c7df809b914293
Seung Woo Kim <koreanboy1120@gmail.com>
2015-08-22 17:17:16 +0900

    Added all text input related scripts.

---
+++ Assets/Script/InGame/ActivateTextAtLine.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateTextAtLine : MonoBehaviour {
+
+	public TextAsset theText;
+
+	public int startLine;
+	public int endLine;
+
+	public TextBoxManager theTextBox;
+
+	public bool requireButtonPress;
+	private bool waitForPress;
+
+	public bool destroyWhenActivated;
+
+	// Use this for initialization
+	void Start () 
+	{
+		theTextBox = FindObjectOfType<TextBoxManager> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (waitForPress && Input.GetKeyDown (KeyCode.J)) {
+			theTextBox.ReloadScript (theText);
+			theTextBox.currentLine = startLine;
+			theTextBox.endAtLine = endLine;
+			theTextBox.EnableTextBox ();
+			
+			if (destroyWhenActivated) {
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player") 
+		{
+			if(requireButtonPress)
+			{
+				waitForPress = true;
+				return;
+			}
+			theTextBox.ReloadScript(theText);
+			theTextBox.currentLine = startLine;
+			theTextBox.endAtLine = endLine;
+			theTextBox.EnableTextBox();
+
+			if (destroyWhenActivated) 
+			{
+				Destroy (gameObject);
+			}
+		}
+	}
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.name == "Player") 
+		{
+			waitForPress = false;
+		}
+	}
+}
---
+++ Assets/Script/InGame/TextBoxManager.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+	public GameObject textBox;
+
+	public Text theText;
+
+	public TextAsset textFiles;
+	public string[] textLines;
+
+	public int currentLine;
+	public int endAtLine;
+
+	public Player player;
+
+	public bool isActive;
+
+	public bool stopPlayerMovement;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = FindObjectOfType<Player> ();
+
+		if (textFiles != null) 
+		{
+			textLines = (textFiles.text.Split ('\n'));
+		}
+
+		if (endAtLine == 0) 
+		{
+			endAtLine = textLines.Length - 1;
+		}
+
+		if (isActive) 
+		{
+			EnableTextBox ();
+		} 
+
+		else 
+		{
+			DisableTextBox ();
+		}
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isActive) 
+		{
+			return;
+		} 
+
+		theText.text = textLines [currentLine];
+		
+		if (Input.GetKeyDown (KeyCode.Return)) 
+		{
+			currentLine += 1;
+		}
+		if (currentLine > endAtLine) 
+		{
+			DisableTextBox ();
+		}
+	}
+	public void EnableTextBox()
+	{
+		textBox.SetActive (true);
+		isActive = true;
+		
+		if (stopPlayerMovement) 
+		{
+			player.canMove = false;
+		}
+	}
+	public void DisableTextBox()
+	{
+		textBox.SetActive (false);
+		isActive = false;
+
+		player.canMove = true;
+	}
+
+	public void ReloadScript(TextAsset theText)
+	{
+		if(theText != null)
+		{
+			textLines = new string[1];
+			textLines = (theText.text.Split ('\n'));
+			
+		}
+
+}
