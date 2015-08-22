44af67649d200dd242de4257bdce7a210d61e5e8
EQwon <bluetwintail0906@gmail.com>
2015-08-22 21:11:28 +0900

    Make Wind Object

---
+++ Assets/Script/InGame/Objects/Wind.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Enums;
+
+public class Wind : MonoBehaviour
+{
+	public WindDirection windDirection;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Global.ingame.inWind = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Global.ingame.inWind = false;
+		}
+	}
+
+}
