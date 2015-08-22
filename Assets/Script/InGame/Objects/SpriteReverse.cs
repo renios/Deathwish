f547648894206d5b5d10d183957f6f021f38fd71
noisum <seomated@naver.com>
2015-08-23 02:21:59 +0900

    Implemented SpriteReverse and Apply new DecayGround Dark Sprite

---
+++ Assets/Script/InGame/Objects/SpriteReverse.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using Enums;
+
+public class SpriteReverse : MonoBehaviour
+{
+	public GravityDirection gravityDirection;
+
+	void Update ()
+	{
+		if(gravityDirection == GravityDirection.Reverse)
+			gameObject.transform.rotation = Quaternion.Euler (180, 0, 0);
+	}
+}
