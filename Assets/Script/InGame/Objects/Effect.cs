731eeed0718e905da5ad04d3d7e859a7b56b45b0
EQwon <bluetwintail0906@gmail.com>
2015-08-26 01:19:21 +0900

    Intergrate Effect Code

---
+++ Assets/Script/InGame/Objects/Effect.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+using Enums;
+
+public class Effect : MonoBehaviour, IRestartable
+{
+	public GameObject Effect_Light;
+	public GameObject Effect_Dark;
+	
+	void Start ()
+	{
+		Effect_Light.SetActive(true);
+		Effect_Dark.SetActive(false);
+	}
+	
+	void Update()
+	{
+		if (Global.ingame.GetIsDarkInPosition(gameObject) == IsDark.Light)
+		{
+			Effect_Light.SetActive(true);
+			Effect_Dark.SetActive(false);
+		}
+		else if (Global.ingame.GetIsDarkInPosition(gameObject) == IsDark.Dark)
+		{
+			Effect_Light.SetActive(false);
+			Effect_Dark.SetActive(true);
+		}
+	}
+	
+	void IRestartable.Restart()
+	{
+		Effect_Light.SetActive(true);
+		Effect_Dark.SetActive(false);
+	}
+}
+
