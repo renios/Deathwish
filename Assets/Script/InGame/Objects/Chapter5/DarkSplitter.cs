9df56d788ba774cdc3d82b23160cf63ef7bc9697
ParkJuhyeong <majecty@gmail.com>
2015-08-25 21:04:02 +0900

    Fix performance bug.
    Does not use FindObjectsWithType every frame.

--- Assets/Script/InGame/Objects/Chapter5/DarkSplitter.cs
+++ Assets/Script/InGame/Objects/Chapter5/DarkSplitter.cs
@@ -4,6 +4,10 @@ using System.Collections;
 namespace Chapter5
 {
 	public class DarkSplitter : MonoBehaviour {
-	
+		public static DarkSplitter Instance = null;
+		void Awake()
+		{
+			Instance = this;
+		}
 	}
 }
