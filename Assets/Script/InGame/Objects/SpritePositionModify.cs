e8315a66cf43ce3930317901f3cfc4016c9e3f36
noisum <seomated@naver.com>
2015-08-28 10:45:10 +0900

    Temporarily add soundeffectcontroller to player

---
+++ Assets/Script/InGame/Objects/SpritePositionModify.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpritePositionModify : MonoBehaviour
+{
+	public float positionModifier;
+	SpriteReverse spriteReverse;
+
+	void Start()
+	{
+		spriteReverse = GetComponent<SpriteReverse> ();
+
+		Vector3 vec = new Vector3 (0, positionModifier, 0);
+		if (spriteReverse.gravityDirection == Enums.GravityDirection.Normal)
+		{
+			gameObject.transform.position -= vec;
+		}
+		else
+		{
+			gameObject.transform.position += vec;
+		}
+	}
+}
