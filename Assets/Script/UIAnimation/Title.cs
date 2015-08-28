8354d9eac0404ae52f5da2c0a959c60a6a6bbdd9
ParkJuhyeong <majecty@gmail.com>
2015-08-28 21:41:06 +0900

    Show death only clear game.

--- Assets/Script/UIAnimation/Title.cs
+++ Assets/Script/UIAnimation/Title.cs
@@ -3,7 +3,11 @@ using System.Collections;
 using System;
 
 public class Title : MonoBehaviour {
+	public SpriteRenderer deathImageRenderer;
+	public bool cheatOn = false;
+
 	void Start () {
+		deathImageRenderer.color -= new Color(1,1,1,0);
 		StartCoroutine(LoadMenu());
 	}
 
@@ -18,12 +22,22 @@ public class Title : MonoBehaviour {
 			spriteRenderer.color += new Color(0.02f, 0.02f, 0.02f, 0);
 			yield return null;
 		}
+		
+		if (SaveLoad.IsAllCleared() || cheatOn)
+		{
+			for (int i=0; i<50; i++)
+			{
+				deathImageRenderer.color += new Color(0.02f, 0.02f, 0.02f, 0);
+				yield return null;
+			}
+		}
 
 		yield return new WaitForSeconds(1.0f);
 
 		for (int i=0; i<50; i++)
 		{
 			spriteRenderer.color -= new Color(0.02f, 0.02f, 0.02f, 0);
+			deathImageRenderer.color -= new Color(0.02f, 0.02f, 0.02f, 0);
 			yield return null;
 		}
 
