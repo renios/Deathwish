84102d18a681515ca3e014918b65595d50d57fda
Minjung Kim <Reny.atkins@gmail.com>
2015-09-13 02:54:26 +0900

    Make mirror effect - blur

--- Assets/Script/InGame/Objects/SwitchDarkLight.cs
+++ Assets/Script/InGame/Objects/SwitchDarkLight.cs
@@ -1,25 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Enums;
+using UnityStandardAssets.ImageEffects;
 
-public class SwitchDarkLight : MonoBehaviour
+public class SwitchDarkLight : MonoBehaviour, IRestartable
 {
 	private bool isGround;
 	private bool isPlayer = false;
+	private bool isItUsedNow = false;
+	private Camera blurEffectCamera;
+	private BlurOptimized blur;
+	IEnumerator mirrorEffectCoroutine;
+
+	void Start()
+	{
+		blur = FindObjectOfType<BlurOptimized>();
+		blurEffectCamera = blur.gameObject.GetComponent<Camera>();
+		blurEffectCamera.enabled = false;
+	}
 
 	void Update()
 	{
 		isGround = GameObject.FindObjectOfType<GroundChecker> ().IsGrounded ();
 
-		if (isPlayer && Input.GetKeyDown(KeyCode.UpArrow) && isGround) //changed for mirror disabling purposes when reading dialogue.
+		if (isPlayer && Input.GetKeyDown(KeyCode.UpArrow) && isGround && (!isItUsedNow)) //changed for mirror disabling purposes when reading dialogue.
 		{
 			if ((FindObjectOfType<TextBoxManager>() != null) && (FindObjectOfType<TextBoxManager>().isActive))
 				return;
-			Global.ingame.ChangeDarkLight();
-			SoundEffectController soundEffectController
-				= GameObject.FindObjectOfType(typeof(SoundEffectController)) as SoundEffectController;
-			soundEffectController.Play(SoundType.Mirror);
+			mirrorEffectCoroutine = PlayMirrorEffect();
+			StartCoroutine(mirrorEffectCoroutine);
 		}
+	}
+
+	IEnumerator PlayMirrorEffect()
+	{
+		isItUsedNow = true;
+		blur.blurSize = 0;
+		blurEffectCamera.enabled = true;
+		
+		SoundEffectController soundEffectController
+			= GameObject.FindObjectOfType(typeof(SoundEffectController)) as SoundEffectController;
+		soundEffectController.Play(SoundType.Mirror);
+		
+		for (int i = 0; i < 50; i++)
+		{
+			blurEffectCamera.GetComponent<BlurOptimized>().blurSize += 0.2f;
+			yield return new WaitForSeconds(0.5f / 50f);
+		}
+		
+		Global.ingame.ChangeDarkLight();
+
+		for (int i = 0; i < 50; i++)
+		{
+			blurEffectCamera.GetComponent<BlurOptimized>().blurSize -= 0.2f;
+			yield return new WaitForSeconds(0.5f / 50f);
+		}
+		
+		blurEffectCamera.enabled = false;
+		isItUsedNow = false;
 	}
 
 	void OnTriggerEnter2D(Collider2D player)
@@ -36,5 +74,13 @@ public class SwitchDarkLight : MonoBehaviour
 		{
 			isPlayer = false;
 		}
+	}
+	
+	void IRestartable.Restart()
+	{
+		if (mirrorEffectCoroutine != null)
+			StopCoroutine(mirrorEffectCoroutine);
+		isItUsedNow = false;
+		blurEffectCamera.enabled = false;
 	}
 }
