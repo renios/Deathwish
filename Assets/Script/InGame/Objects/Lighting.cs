762036c2d193a3af108f223b0e52c865ca17852f
Minjung Kim <Reny.atkins@gmail.com>
2015-09-13 20:00:11 +0900

    Delete unused values

--- Assets/Script/InGame/Objects/Lighting.cs
+++ Assets/Script/InGame/Objects/Lighting.cs
@@ -10,8 +10,6 @@ public class Lighting: MonoBehaviour, IRestartable
 	public int startDelay;
 	public int lightTerm;
 	public int darkTerm;
-	public new Sprite light;
-	public Sprite dark;
 	public GameObject lightningEffect;
 
 	public bool isLighting;
@@ -33,7 +31,6 @@ public class Lighting: MonoBehaviour, IRestartable
 	void changeLight()
 	{
 		isLighting = true;
-		GetComponent<SpriteRenderer> ().sprite = light;
 		SoundEffectController soundEffectController 
 			= GameObject.FindObjectOfType(typeof(SoundEffectController)) as SoundEffectController;
 		soundEffectController.Play (SoundType.Lightning);
@@ -43,11 +40,11 @@ public class Lighting: MonoBehaviour, IRestartable
 	void changeDark()
 	{
 		isLighting = false;
-		GetComponent<SpriteRenderer> ().sprite = dark;
 	}
 
 	void IRestartable.Restart()
 	{
+		// It strange that changing global value here.
 		Global.ingame.isDark = IsDark.Light;
 		Destroy(effect);
 	}
