c685afe360eef2f8cd211a2ac26e7999a919ffc1
EQwon <bluetwintail0906@gmail.com>
2015-09-16 20:41:07 +0900

    Create WindEffectController.cs
    for proper Wind Effect

---
+++ Assets/Script/InGame/Objects/WindEffectController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Enums;
+
+public class WindEffectController : MonoBehaviour
+{
+    private WindDirection windDirection;
+
+    void Start()
+    {
+        GetComponent<Wind>().windDirection = windDirection;
+
+        if (windDirection == WindDirection.Left)
+        {
+            transform.Rotate(0, 270, 270);
+        }
+        else if (windDirection == WindDirection.Right)
+        {
+            transform.Rotate(0, 90, 270);
+        }
+        else if (windDirection == WindDirection.Up)
+        {
+            transform.Rotate(0, 270, 270);
+        }
+        else if (windDirection == WindDirection.Down)
+        {
+            transform.Rotate(0, 90, 270);
+        }
+    }
+}
